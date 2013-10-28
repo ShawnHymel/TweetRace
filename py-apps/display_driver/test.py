9af7587577d084d85641167900c3e93ac91630c5
Raspberry Pi <raspberry@pi.com>
2013-10-28 16:58:14 -0600

    Added code for all 5 displays

--- py-apps/display_driver/test.py
+++ py-apps/display_driver/test.py
@@ -1,7 +1,11 @@
+import time
 import display_driver
 
 # Parameters
-out_strs = ["HashTest123", "MileyCyrusTrainWreck", "OMG", "This is", "A test"]
+out_strs = ["HashTest123", "MileyCyrus", "OMG", "Thisis", "test"]
+
+# Create display object
+disp_p = display_driver.display_driver()
 
 # Loop through each string
 for str_index, s in enumerate(out_strs):   
@@ -10,14 +14,20 @@ for str_index, s in enumerate(out_strs):
     out_chars = display_driver.new_charArray(len(s))
 
     # Fill char array
-    print 'String ', str_index, ' : ', s
+    print 'String ', str_index, ': ', s
     for i, c in enumerate(bytearray(s.upper())):
         display_driver.charArray_setitem(out_chars,i,c)
+        print i, ' : ', c
 
-    # Create display driver object and send out string
-    disp_p = display_driver.display_driver()
+    # Send out string
     if not disp_p.update_string(str_index, out_chars):
         print 'Update string failed'
+
+    print out_chars
+    display_driver.delete_charArray(out_chars)
+
+# Sleep to let displays cycle
+time.sleep(30)
 
 # Cleanup
 # display_driver.delete_charArray(out_chars)
