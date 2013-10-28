3f8c76d0fdb85cf14c5471303912ff6e01b2250c
sgmustadio <sgmustadio@lubuntu-VirtualBox.(none)>
2013-10-28 23:13:16 +0100

    Updated display test to include 5 strings

--- py-apps/display_driver/test.py
+++ py-apps/display_driver/test.py
@@ -1,22 +1,23 @@
 import display_driver
 
 # Parameters
-spidev = "/dev/spidev0.0"
-out_str = "HashTest123"
-index = 0
+out_strs = ["HashTest123", "MileyCyrusTrainWreck", "OMG", "This is", "A test"]
 
-# Define char array
-out_chars = display_driver.new_charArray(len(out_str))
+# Loop through each string
+for str_index, s in enumerate(out_strs):   
 
-# Fill char array
-for i, c in enumerate(bytearray(out_str.upper())):
-    print i, ' : ', c
-    display_driver.charArray_setitem(out_chars,i,c)
+    # Define char array
+    out_chars = display_driver.new_charArray(len(s))
 
-# Create display driver object and send out string
-disp_p = display_driver.display_driver()
-if not disp_p.update_string(index, out_chars):
-    print 'Update string failed'
+    # Fill char array
+    print 'String ', str_index, ' : ', s
+    for i, c in enumerate(bytearray(s.upper())):
+        display_driver.charArray_setitem(out_chars,i,c)
+
+    # Create display driver object and send out string
+    disp_p = display_driver.display_driver()
+    if not disp_p.update_string(str_index, out_chars):
+        print 'Update string failed'
 
 # Cleanup
 # display_driver.delete_charArray(out_chars)
