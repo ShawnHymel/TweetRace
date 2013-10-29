1c0e2246866d7d086368e006b14f8861d21fccd3
ShawnHymel <shawn.hymel@sparkfun.com>
2013-10-29 01:06:19 +0100

    Added null chars to strings in Python display test

--- py-apps/display_driver/test.py
+++ py-apps/display_driver/test.py
@@ -2,56 +2,28 @@ import time
 import display_driver
 
 # Parameters
-out_strs = ["HashTest123", "MileyCyrus", "OMG", "Thisis", "test"]
-
-# Declare character buffers
-print out_strs[1]
-out_chars = [   display_driver.new_charArray(len(out_strs[0])),
-                display_driver.new_charArray(len(out_strs[1])),
-                display_driver.new_charArray(len(out_strs[2])),
-                display_driver.new_charArray(len(out_strs[3])),
-                display_driver.new_charArray(len(out_strs[4])) ]
+out_strs = ["HashTest123\0", "MileyCyrus\0", "OMG\0", "Thisis\0", "test\0"]
 
 # Create display object
 disp_p = display_driver.display_driver()
 
-# Fill character buffers and send to SPI
-for str_index, s in enumerate(out_strs):
-    print 'String ', str_index, ': ', s
-    for ch_index, c in enumerate(bytearray(s.upper())):
-        display_driver.charArray_setitem(out_chars[str_index], ch_index, c)
-        print ch_index, ' : ', c
-    if not disp_p.update_string(str_index, out_chars[str_index]):
-        print 'Update string failed'
-
-# Create display object
-# disp_p = display_driver.display_driver()
-
-# t1 = display_driver.new_charArray(10)
-# display_driver.delete_charArray(t1)
-
 # Loop through each string
-# for str_index, s in enumerate(out_strs):   
+for str_index, s in enumerate(out_strs):   
 
     # Define char array
-#    out_chars = display_driver.new_charArray(len(s))
-#    display_driver.delete_charArray(out_chars)
+    out_chars = display_driver.new_charArray(len(s))
 
     # Fill char array
-#    print 'String ', str_index, ': ', s
-#    for i, c in enumerate(bytearray(s.upper())):
-#        display_driver.charArray_setitem(out_chars,i,c)
-#        print i, ' : ', c
+    print 'String ', str_index, ': ', s
+    for i, c in enumerate(bytearray(s.upper())):
+        display_driver.charArray_setitem(out_chars,i,c)
+        print i, ' : ', display_driver.charArray_getitem(out_chars,i)
 
     # Send out string
-#    if not disp_p.update_string(str_index, out_chars):
-#        print 'Update string failed'
+    if not disp_p.update_string(str_index, out_chars):
+        print 'Update string failed'
 
-#    print out_chars
-
+    #display_driver.delete_charArray(out_chars)
 
 # Sleep to let displays cycle
-time.sleep(30)
-
-# Cleanup
-# display_driver.delete_charArray(out_chars)
+time.sleep(1)
