2d5cc02db9695d7d08152e235306ed8a587e876a
ShawnHymel <shawn.hymel@sparkfun.com>
2013-10-30 20:06:19 +0100

    Initial config in Tweetrace

--- TweetRace.py
+++ TweetRace.py
@@ -1,14 +1,66 @@
-import os
-print __file__
-dir = os.path.dirname(__file__)
-filename = os.path.join(dir, '/py_apps/twit_feed')
-
+import os, sys
 import time
+import ConfigParser
+
+#-----------------------------------------------------------------------------
+# Import custom modules
+#-----------------------------------------------------------------------------
+
+# Add display_driver module to path
+path = os.path.join(os.path.dirname(__file__), 'py_apps/display_driver')
+sys.path.append(path)
+
+# Add motor_driver module to path
+path = os.path.join(os.path.dirname(__file__), 'py_apps/motor_driver')
+sys.path.append(path)
+
+# Add twit_feed module to path
+path = os.path.join(os.path.dirname(__file__), 'py_apps/twit_feed')
+sys.path.append(path)
+
+import display_driver
+import motor_driver
 import twit_feed
 
-# Game parameters
-terms = ['#omg', '#hello', '#diy', '#robot', '#fun']    # Search terms
-game_time = 10                                          # Game time (seconds)
+#-----------------------------------------------------------------------------
+# Constants
+#-----------------------------------------------------------------------------
+
+PARAM_FILE = 'param.txt'
+
+#-----------------------------------------------------------------------------
+# Global Variables
+#-----------------------------------------------------------------------------
+
+g_terms = None
+g_game_time = None
+
+#-----------------------------------------------------------------------------
+# Functions
+#-----------------------------------------------------------------------------
+
+# Automatically read the parameters text file and configure the game settings.
+def config_params():
+    
+    global g_terms
+    global g_game_time
+
+    # Open text file
+    config = ConfigParser.RawConfigParser()
+    config.read(PARAM_FILE)
+
+    # Read in twitter authentication
+    print config.get('twitter_auth', 'app_key')
+    print config.get('twitter_auth', 'app_secret')
+    print config.get('twitter_auth', 'oauth_token')
+    print config.get('twitter_auth', 'oauth_token_secret')
+
+    # Read in game settings
+    g_terms = config.get('game_settings', 'terms')
+    g_game_time = float(config.get('game_settings', 'game_time'))
+
+config_params()
+print g_terms
 
 # Twitter authentication
 twitter_auth = {    'app_key': '0SGK64SYL8Y0R4tCbcm6SA',
@@ -17,11 +69,11 @@ twitter_auth = {    'app_key': '0SGK64SYL8Y0R4tCbcm6SA',
                     'oauth_token_secret': 'gv9vGWluxgMtMY77evxainwolbFhxIiCJsGTd79N2I' }
 
 # Create a TwitFeed object
-tf = twit_feed.TwitFeed(twitter_auth, terms)
+tf = twit_feed.TwitFeed(twitter_auth, g_terms)
 
 # Wait for game to end and display tweets/score
 print 'START OF GAME!'
-time.sleep(game_time)
+time.sleep(g_game_time)
 tweet_list = tf.get_tweets()
 for m in tweet_list:
     print m, '\n'
@@ -30,7 +82,7 @@ print tf.tally_tweets()
 
 # Try another game
 print 'START OF GAME!'
-time.sleep(game_time)
+time.sleep(g_game_time)
 tweet_list = tf.get_tweets()
 for m in tweet_list:
     print m, '\n'
